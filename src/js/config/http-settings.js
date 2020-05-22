e1394d2553612e53da96213f6b951fb9ef4bd9dc
zhangqc <zhangqc@asiainfo.com>
2020-05-23 01:46:12 +0800

    华为地址切换

--- src/js/config/http-settings.js
+++ src/js/config/http-settings.js
@@ -1,7 +1,7 @@
 //本地
-export const baseHttpUrl = "http://localhost:8085/steward";
+// export const baseHttpUrl = "http://localhost:8085/steward";
 //花生壳
-// export const baseHttpUrl = "http://lucky.eicp.top:14296/steward";
-//华为服务器
-// export const baseHttpUrl = "http://139.9.135.26:8085/steward";
+export const baseHttpUrl = "http://lucky.eicp.top:14296/steward";
+华为服务器
+export const baseHttpUrl = "http://139.9.135.26:8085/steward";
 export const httpTimeOut = 60000;
