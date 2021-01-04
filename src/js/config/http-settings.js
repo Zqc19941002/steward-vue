540cc06431fb744d6e54b9729865533d9cfc38c2
zhangqc <zhangqc@asiainfo.com>
2021-01-04 16:45:58 +0800

    配置修改

--- src/js/config/http-settings.js
+++ src/js/config/http-settings.js
@@ -1,8 +1,8 @@
 //本地
-export const baseHttpUrl = "http://localhost:8085/steward";
+// export const baseHttpUrl = "http://192.168.0.111:8085/steward";
 //花生壳
 // export const baseHttpUrl = "http://lucky.eicp.top:14296/steward";
 // 华为服务器
-// export const baseHttpUrl = "http://139.9.135.26:8085/steward";
+export const baseHttpUrl = "http://www.october2.top:8085/steward";
 
 export const httpTimeOut = 60000;
